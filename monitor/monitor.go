0c710c3bbfd3c9e92ab199619d9dee40229595bd
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2017-07-17 21:13:37 -0700

    Parse monitor options (just start for now)

--- monitor/monitor.go
+++ monitor/monitor.go
@@ -3,6 +3,7 @@ package monitor
 ////////////////////////////////////////////////////////////////////////////////
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
@@ -12,12 +13,35 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////
 
+type monitorOpts struct {
+	start bool
+}
+
+func parseMonitorArgs(opts []string) (*monitorOpts, error) {
+	args := &monitorOpts{}
+	fs := flag.NewFlagSet("monitor", flag.ContinueOnError)
+	fs.BoolVar(&args.start, "start", false, "start the coin daemon if it is not running")
+	if err := fs.Parse(opts); err != nil {
+		return nil, err
+	}
+	return args, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 type Monitor struct {
 	Coin *coin.Coin
 	CLI  *types.CLI
+	Opts *monitorOpts
 }
 
 func New(cli *types.CLI, opts []string) (*Monitor, error) {
+	// Parse monitor specific arguments.
+	mopts, err := parseMonitorArgs(opts)
+	if err != nil {
+		return nil, err
+	}
+
 	// What coin are we monitoring?
 	c, err := coin.GetCoinByName(cli.Coin)
 	if err != nil {
@@ -33,6 +57,7 @@ func New(cli *types.CLI, opts []string) (*Monitor, error) {
 	return &Monitor{
 		Coin: c,
 		CLI:  cli,
+		Opts: mopts,
 	}, nil
 }
 
